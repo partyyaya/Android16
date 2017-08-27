2ef840a4a6405279d69d92112cdbdf8382b2ebe4
LBJ <LBJ>
2017-08-27 11:06:15 +0800

    20170827

--- app/src/main/java/tw/ming/app/helloworid/myfragment/F1Fragment.java
+++ app/src/main/java/tw/ming/app/helloworid/myfragment/F1Fragment.java
@@ -7,8 +7,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class F1Fragment extends Fragment {
+    private TextView mesg;
+    private View mainView;
+    private MainActivity mainActivity;
+    private TextView mainTitle;
     public F1Fragment(){
         Log.i("ming","F1Fragment");
     }
@@ -23,25 +28,71 @@ public class F1Fragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("ming","F1Fragment:onCreate()");
+        mainActivity = (MainActivity) getActivity();
+        mainTitle = mainActivity.getMainTitle();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f1, container, false);
+        // TODO: Rename method, update argument and hook method into UI event
+        //Log.i("ming","F1Fragment:onCreateView()");
+        //container部等於R.id.container
+        if(mainView==null) {
+            mainView = inflater.inflate(R.layout.fragment_f1, container, false);
+            mesg = (TextView) mainView.findViewById(R.id.f1mesg);
+            //若按下按鈕則看自己
+            final View f1test1 = mainView.findViewById(R.id.f1test1);
+            f1test1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    f1test1();
+                }
+            });
+
+            final View f1test2 = mainView.findViewById(R.id.f1test2);
+            f1test2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    f1test2();
+                }
+            });
+            final View f1test3 = mainView.findViewById(R.id.f1test3);
+            f1test3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    f1test3();
+                }
+            });
+        }
+        return mainView;
+    }
+    public void f1test1(){//會叫步道
+        mesg.setText(""+(int)(Math.random()*40));
+    }
+
+    public void f1test2(){
+        //Log.i("ming","f2:ok"+(int)(Math.random()*40));
+        mainTitle.setText("TitleChange");
+    }
+
+    public void f1test3(){
+        TextView f2mesg = mainActivity.getF2().getF2mesg();
+        if (f2mesg != null){
+            f2mesg.setText("Change by F1");
+        }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        Log.i("ming","onAttach()");
+        Log.i("ming","F1:onAttach()");
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        Log.i("ming","onDetach()");
+        Log.i("ming","F1:onDetach()");
     }
 
 }
--- app/src/main/java/tw/ming/app/helloworid/myfragment/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/myfragment/MainActivity.java
@@ -1,13 +1,48 @@
 package tw.ming.app.helloworid.myfragment;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private F1Fragment f1;
+    private F2Fragment f2;
+    private FragmentManager fmgr;
+    private FragmentTransaction tran;
+    private boolean isf2;
+    private TextView title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        f1 = F1Fragment.newInstance();
+        f2 = new F2Fragment();
+
+        title = (TextView) findViewById(R.id.title);
+
+        fmgr = getFragmentManager();
+        tran = fmgr.beginTransaction();
+        tran.add(R.id.container,f1);//將f1放進來
+        tran.commit();
+    }
+    public void change(View view){
+        isf2 = !isf2;
+        tran = fmgr.beginTransaction();
+        tran.replace(R.id.container,isf2?f2:f1);//切換到版面
+        tran.addToBackStack(null);//上一頁(back)
+        tran.commit();
+    }
+
+//    public void f1test1(View view){
+//        Log.i("ming","ok");
+//    }
+public TextView getMainTitle(){return title;}
+    public F2Fragment getF2(){
+        return f2;
     }
 }
